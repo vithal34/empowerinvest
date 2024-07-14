74c0b43515d255b8e55370c997db6fe69a7f268f
Vithal Agrawal <59925019+vithal34@users.noreply.github.com>
2024-07-15 01:02:24 +0530

    Update vite.config.js

--- app/vite.config.js
+++ app/vite.config.js
@@ -3,16 +3,16 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		host: "0.0.0.0",
-		proxy: {
-			"/api": {
-				target: "http://0.0.0.0:3010",
-				changeOrigin: false,
-				secure: false,
-				ws: true,
-			},
-		},
-	},
+  plugins: [react()],
+  server: {
+    host: "0.0.0.0",
+    proxy: {
+      "/api": {
+        target: "https://empowerinvest-back.onrender.com", // Change the target URL here
+        changeOrigin: true, // Set this to true if you are proxying to a different domain
+        secure: false, // Set this to false if your target URL uses HTTP instead of HTTPS
+        ws: true, // Enable WebSocket proxying
+      },
+    },
+  },
 });
